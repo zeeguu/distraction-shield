d038ae72c2185fb6235f27fb57966cf447f43ba4
Mark <marktmmrmn@gmail.com>
2017-03-02 22:38:40 +0100

    Added some clarifying comments to the blacklist.js file.

--- sampleExtension/blacklist.js
+++ sampleExtension/blacklist.js
@@ -1,6 +1,6 @@
-/**
- * Created by mark on 2-3-17.
- */
+
+// Every document load, this function is being run. It reads the local storage for the "blacklist" element,
+// and appends the urls to the url list.
 document.body.onload = function() {
     chrome.storage.local.get("blacklist", function(items) {
         if (!chrome.runtime.error) {
@@ -11,33 +11,45 @@ document.body.onload = function() {
     });
 };
 
+// Whenever the save button is clicked, this function takes the text inside the input field, and saves it
+// to the "blacklist" element in the local storage.
 document.getElementById("save").onclick = function() {
     var urlelement = $('#text');
     var newurl = urlelement.val();
     chrome.storage.local.get("blacklist", function (output) {
         var links = output.blacklist;
+
+        // Add the url to the local storage.
         links.push("*://"+newurl+"/*");
         chrome.storage.local.set({"blacklist" : links }, function() {
-            if (chrome.runtime.error) {
+            if(chrome.runtime.error) {
                 console.log("Runtime error.");
             }
-            $('#blacklist').append($("<option></option>").text(newurl));
+            $('#blacklist').append($("<option></option>").text("*://"+newurl+"/*"));
+
+            // Empty the input box.
             urlelement.val('');
         });
     });
 };
 
+// Whenever an url is selected, and the delete button is clicked, this function deletes the element from the list
+// and from the local storage.
 document.getElementById("delete").onclick = function() {
     var urltodelete = $("#blacklist option:selected");
     var urlkey = urltodelete.attr("key");
 
     chrome.storage.local.get("blacklist", function (output) {
         var links = output.blacklist;
+
+        // Remove the url from the local storage.
         links.splice(urlkey, 1);
         chrome.storage.local.set({"blacklist" : links }, function() {
-            if (chrome.runtime.error) {
+            if(chrome.runtime.error) {
                 console.log("Runtime error.");
             }
+
+            // Remove the url from the list.
             urltodelete.remove();
         });
     });
