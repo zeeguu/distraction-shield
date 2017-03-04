c91a10ddda18b0b3e855bb43f4824a27011a8c02
pieterDekker <pieter.dekker20@gmail.com>
2017-03-04 13:49:51 +0100

    first working iframe overlay containing an exercise

--- sampleExtension/insertIframe.js
+++ sampleExtension/insertIframe.js
@@ -1,9 +1,8 @@
 /**
  * Created by Eli Ionescu on 3/3/2017.
  */
-// $(document).showPopup("<iframe src=\" http://zeeguu.herokuapp.com/get-ex\" width=\"700\" height=\"530\" seamless=\"\"></iframe>");
+
 var popupDiv = $('<div data-role="popup" id="dialogMsg" >' +
     '<iframe src=" https://zeeguu.herokuapp.com/get-ex" width="800" height="600" seamless=""></iframe>' +
     '</div>');
-$("body").prepend(popupDiv);
-$.getScript(chrome.extension.getURL("iframe.js"));
+$("body").append(popupDiv);
