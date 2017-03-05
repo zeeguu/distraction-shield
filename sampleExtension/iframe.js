ee79f78c3fbfef7c877044ef324634222e93898c
Edser Apperloo <gamesedser@hotmail.com>
2017-03-05 15:03:35 +0100

    added some features to the popup, like disabling background

--- sampleExtension/iframe.js
+++ sampleExtension/iframe.js
@@ -1,6 +1,14 @@
+
+
 $(function () {
     $('#dialogMsg').dialog(
-        { width : 900,
-            height: 650 }
+        {	width: 835,
+          	height: 665,  	 
+          	resizable: false,
+          	modal: true,
+          	draggable: false,
+          	closeOnEscape: false,
+          	dialogClass: "no-close"
+      	}
     );
 });
