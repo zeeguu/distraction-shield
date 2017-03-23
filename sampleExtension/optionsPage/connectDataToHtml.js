937db76fc83b4ef62f401720f5346b4b88abcf56
Edser Apperloo <gamesedser@hotmail.com>
2017-03-23 12:50:41 +0100

    refactored version of options, cleaned out the branch and added the radiobutton functionality

---
+++ sampleExtension/optionsPage/connectDataToHtml.js
@@ -0,0 +1,26 @@
+/**
+ * This file connects the data we read from storage and saved in local variables
+ * to the actual html representations of this data.
+ */
+
+loadHtmlBlacklist = function(list) {
+    //For each BlockedSite object from the list generate a tableRow
+    $.each(list, function(key, value) {
+        appendHtmlItemTo(generateHtmlTableRow(value), html_table);
+    });
+    sortHtmlOnChecked(html_table);
+};
+
+loadHtmlMode = function(extensionMode) {
+    $("input[name=modeOptions][value=" + extensionMode + "]").prop('checked', true);
+};
+
+loadHtmlInterceptCounter = function(count) {
+    html_intCnt = count;
+};
+
+connectLocalDataToHtml = function() {
+    loadHtmlInterceptCounter(interceptionCounter);
+    loadHtmlBlacklist(links);
+    loadHtmlMode(mode);
+};
