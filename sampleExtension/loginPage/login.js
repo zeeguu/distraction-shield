8086194b0077979b09e85802c4c667582bb1cbfd
Mark <marktmmrmn@gmail.com>
2017-03-29 13:53:32 +0200

    Created very basic sign-in functionality.

--- sampleExtension/loginPage/login.js
+++ sampleExtension/loginPage/login.js
@@ -5,27 +5,62 @@ var console = chrome.extension.getBackgroundPage().console;
 
 /* -------------------- -------------------------- -------------------- */
 var html_loginButton = $('#loginBtn');
-var html_usernameFld = $('#usernameFld');
-var html_passwordFld = $('#passwordFld');
+var html_usernameLoginFld = $('#usernameLoginFld');
+var html_passwordLoginFld = $('#passwordLoginFld');
+
+var html_signinButton = $('#signinBtn');
+var html_usernameSigninFld = $('#usernameSigninFld');
+var html_passwordSigninFld = $('#passwordSigninFld');
+
+var loginUrl = "https://zeeguu.unibe.ch/api/session";
+var signinUrl = "https://zeeguu.unibe.ch/api/add_user";
+
+var loginAnonUrl = "https://zeeguu.unibe.ch/api/get_anon_session";
+var signinAnonUrl = "https://zeeguu.unibe.ch/api/add_anon_user";
 
 loginZeeguu = function() {
-    var username = html_usernameFld.val();
-    var password = html_passwordFld.val();
+    var username = html_usernameLoginFld.val();
+    var password = html_passwordLoginFld.val();
 
     console.log("Username:"+username+" Password:"+password);
 
-    html_usernameFld.val('');
-    html_passwordFld.val('');
+    html_usernameLoginFld.val('');
+    html_passwordLoginFld.val('');
 
     $.ajax({
         type: "POST",
-        url: "https://zeeguu.unibe.ch/api/get_anon_session/"+username,
+        url: loginAnonUrl+"/"+username,
         data: $.param({
             //uuid: username,
             password: password
         })
-    }).done(function(data){
-        console.log(data);
+    }).done(function(session){
+        console.log(session);
+    }).fail(function(data){
+        console.log("Fail..");
+    }).always(function(data){
+        console.log("always? "+data);
+    });
+};
+
+signinZeeguu = function() {
+    var username = html_usernameSigninFld.val();
+    var password = html_passwordSigninFld.val();
+
+    console.log("Username:"+username+" Password:"+password);
+
+    html_usernameSigninFld.val('');
+    html_passwordSigninFld.val('');
+
+    $.ajax({
+        type: "POST",
+        url: signinAnonUrl,
+        data: $.param({
+            uuid: username,
+            password: password
+        })
+    }).done(function(session){
+        console.log(session);
     }).always(function(data){
         console.log("always? "+data);
     });
@@ -38,6 +73,7 @@ connectButton = function(html_button, method) {
 
 connectHtmlFunctionality = function() {
     connectButton(html_loginButton, loginZeeguu);
+    connectButton(html_signinButton, signinZeeguu);
 };
 
 //Run this when the page is loaded.
