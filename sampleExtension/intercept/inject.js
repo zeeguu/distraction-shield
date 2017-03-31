502b868ff345546f47a9b61fa3899ef2146aa9c1
pieterDekker <pieter.dekker20@gmail.com>
2017-03-31 11:55:28 +0200

    html parsing added

--- sampleExtension/intercept/inject.js
+++ sampleExtension/intercept/inject.js
@@ -1,8 +1,4 @@
-console.log("inject.js");//TODO remove
-
-
 mainFlow = function() {
-    console.log("mainFlow");
     getStorageMode (initBasis);
 };
 
@@ -12,12 +8,9 @@ initForMode = function(mode) {
     var message;
     if (mode == "pro" || mode == undefined) {
         message = proText;
-        // initPro();
     } else if(mode == "lazy"){
         message = lazyText;
-        // initLazy();
     }
-    console.log ("initialized "+mode+" mode.");//TODO remove
     return message;
 };
 
@@ -29,58 +22,24 @@ initBasis = function(mode) {
         type: "GET",
         timeout: 5000,
         datattype: "html",
-        success: function(data) {
+        success: function (data) {
             infoDiv = $.parseHTML(data);
+            $("body").prepend(infoDiv);
+            $("#tds").width(window.innerWidth + "px");
             $("#tds_generalInfoText").append(infoText);
             $("#tds_modeSpecificText").append(message);
-
-            // infoDiv.append(infoP).append(specificP);
-            $("body").prepend(infoDiv);
+            $("#originalDestination").attr("href", getDest());
         },
     });
+}
 
-
-    // infoDiv = $.parseHTML(chrome.extension.getURL('intercept/inject.html'), );
-    // $("#tds_generalInfoText").append(infoText);
-    // $("#tds_modeSpecificText").append(message);
-    //
-    // // infoDiv.append(infoP).append(specificP);
-    // $("body").prepend(infoDiv);
-};
-
-/*initialize lazy mode*/
-
-initLazy = function() {
-    var lazyDiv =  $("\<div id='tds_lazyDiv'></div>");
-    var skipButton = $("<button id='tds_skipButton' class='ui-button ui-corner-all ui-widget'> I'm lazy and I want to skip</button>");
-    skipButton.on("click", function () {
-        chrome.storage.sync.get("originalDestination", function (url) {
-            chrome.runtime.sendMessage({message: "goToOriginalDestination", destination: url.originalDestination});
-        });
-    });
-    lazyDiv.append(skipButton);
-    $(".ex-container").prepend(lazyDiv);
-};
-
-/*initialize pro mode*/
-
-initPro = function() {
-    chrome.storage.sync.get("originalDestination", function (url) {
-        /* after receiving the original destiniation we attach some code to zeeguu
-           This will make sure
-         */
-        var destination = url.originalDestination;
-        var defBindEvents = document.createElement('script');
-        defBindEvents.src = chrome.extension.getURL('intercept/bindEvents.js');
-        defBindEvents.onload = function() {
-            var callBindEvents = document.createElement('script');
-            callBindEvents.textContent = '(subToCompletedEvent)(' + JSON.stringify(destination) + ');';
-            (document.head || document.documentElement).appendChild(callBindEvents);
-            this.remove();
-        };
-        (document.head || document.documentElement).appendChild(defBindEvents);
-    });
-
-};
+getDest = function() {
+    url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]redirect(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results || !results[2]) return null;
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 mainFlow();
