984339c009ee5f43b34414dee687c1cbf281786c
Eli Ionescu <ionescuelena95@gmail.com>
2017-05-11 16:08:55 +0200

    ES6: tooltip

--- Distraction Shield/tooltipPage/tooltip.js
+++ Distraction Shield/tooltipPage/tooltip.js
@@ -1,36 +1,12 @@
-require.config({
-    baseUrl: "./",
-    paths : {
-        'BlockedSite'               : '../classes/BlockedSite',
-        'BlockedSiteList'           : '../classes/BlockedSiteList',
-        'UserSettings'              : '../classes/UserSettings',
-        'exerciseTime'              : '../modules/statistics/exerciseTime',
-        'interception'              : '../modules/statistics/interception',
-        'tracker'                   : '../modules/statistics/tracker',
-        'blockedSiteBuilder'        : '../modules/blockedSiteBuilder',
-        'dateutil'                  : '../modules/dateutil',
-        'storage'                   : '../modules/storage',
-        'synchronizer'              : '../modules/synchronizer',
-        'urlFormatter'              : '../modules/urlFormatter',
-        'background'                : '../background',
-        'constants'                 : '../constants',
-        'BlacklistStatsTable'       : '../statisticsPage/classes/BlacklistStatsTable',
-        'ExerciseTimeTable'         : '../statisticsPage/classes/ExerciseTimeTable',
-        'InterceptionCounterTable'  : '../statisticsPage/classes/InterceptionCounterTable',
-        'jquery'                    : '../dependencies/jquery/jquery-1.10.2',
-        'domReady'                  : '../domReady'
+import * as $ from "../../dependencies/jquery/jquery-1.10.2";
 
-    }
-});
-
-require(['jquery'], function($) {
-    var saveButton = $('#saveBtn');
-    var optionsButton = $('#optionsBtn');
-    var statisticsButton = $('#statisticsBtn');
+    let saveButton = $('#saveBtn');
+    let optionsButton = $('#optionsBtn');
+    let statisticsButton = $('#statisticsBtn');
 
     saveCurrentPageToBlacklist = function () {
         chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
-            var activeTab = arrayOfTabs[0];
+            let activeTab = arrayOfTabs[0];
             chrome.runtime.sendMessage({message : "newUrl", unformattedUrl : activeTab.url}, setSaveButtonToSuccess());
         });
     };
@@ -53,14 +29,12 @@ require(['jquery'], function($) {
     };
 
 
-//Connect functions to HTML elements
+    //Connect functions to HTML elements
     connectButtons = function () {
         saveButton.on('click', saveCurrentPageToBlacklist);
         optionsButton.on('click', openOptionsPage);
         statisticsButton.on('click', redirectToStatistics);
     };
 
-
     connectButtons();
-});
 
