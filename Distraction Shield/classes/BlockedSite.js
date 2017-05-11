af7576ea687fd420838f01ac8faa6ebfa9c45502
pieterDekker <pieter.dekker20@gmail.com>
2017-05-11 13:02:52 +0200

    blockedsite is es6 now

--- Distraction Shield/classes/BlockedSite.js
+++ Distraction Shield/classes/BlockedSite.js
@@ -1,115 +1,83 @@
-define('BlockedSite', ['constants'], function BlockedSite(constants) {
+import * as constants from '/constants';
 
-    /* --------------- --------------- Serialization --------------- --------------- */
-    //Private to this and storage.js
-    serializeBlockedSite = function (blockedSite) {
-        var obj = {
-            url: blockedSite.getUrl(),
-            domain: blockedSite.getDomain(),
-            name: blockedSite.getName(),
-            icon: blockedSite.getIcon(),
-            checkboxVal: blockedSite.getCheckboxVal(),
-            counter: blockedSite.getCounter(),
-            timeSpent: blockedSite.getTimeSpent()
-        };
-        return JSON.stringify(obj);
+/* --------------- --------------- Serialization --------------- --------------- */
+//Private to this and storage.js
+export function serializeBlockedSite (blockedSite) {
+    var obj = {
+        url: blockedSite.getUrl(),
+        domain: blockedSite.getDomain(),
+        name: blockedSite.getName(),
+        icon: blockedSite.getIcon(),
+        checkboxVal: blockedSite.getCheckboxVal(),
+        counter: blockedSite.getCounter(),
+        timeSpent: blockedSite.getTimeSpent()
     };
+    return JSON.stringify(obj);
+};
 
-    //Private to this and blocked_site_list
-    parseBlockedSite = function (blockedSite) {
-        var b = new BlockedSite();
-        b.setUrl(blockedSite.url);
-        b.setDomain(blockedSite.domain);
-        b.setName(blockedSite.name);
-        b.setIcon(blockedSite.icon);
-        b.setCheckboxVal(blockedSite.checkboxVal);
-        b.setCounter(blockedSite.counter);
-        b.setTimeSpent(blockedSite.timeSpent);
-        return b;
-    };
+//Private to this and blocked_site_list
+export function parseBlockedSite (blockedSite) {
+    var b = new BlockedSite();
+    b.setUrl(blockedSite.url);
+    b.setDomain(blockedSite.domain);
+    b.setName(blockedSite.name);
+    b.setIcon(blockedSite.icon);
+    b.setCheckboxVal(blockedSite.checkboxVal);
+    b.setCounter(blockedSite.counter);
+    b.setTimeSpent(blockedSite.timeSpent);
+    return b;
+};
 
-    //Private to this and storage.js
-    deserializeBlockedSite = function (serializedBlockedSite) {
-        if (serializedBlockedSite != null) {
-            var parsed = JSON.parse(serializedBlockedSite);
-            return parseBlockedSite(parsed);
-        }
-        return null;
-    };
+//Private to this and storage.js
+export function deserializeBlockedSite (serializedBlockedSite) {
+    if (serializedBlockedSite != null) {
+        var parsed = JSON.parse(serializedBlockedSite);
+        return parseBlockedSite(parsed);
+    }
+    return null;
+};
 
-    /* --------------- --------------- --------------- --------------- --------------- */
+/* --------------- --------------- --------------- --------------- --------------- */
 
-    function BlockedSite(urlBase, title) {
-        constructUrl = function (url) {
-            return "*://" + url + "/*";
-        };
+export class BlockedSite {
 
-        constructIcon = function (url) {
-            return "<img style=\"-webkit-user-select: none\" src=\"" + constants.FAVICONLINK + url + "\">"
-        };
-
-        var url = constructUrl(urlBase);
-        var domain = urlBase;
-        var name = title;
-        var icon = constructIcon(urlBase);
-        var checkboxVal = true;
-        var counter = 0;
-        var timeSpent = 0;
-
-        this.getUrl = function () {
-            return url;
-        };
-        this.setUrl = function (newUrl) {
-            url = newUrl;
-        };
-        this.getDomain = function () {
-            return domain;
-        };
-        this.setDomain = function (newDomain) {
-            domain = newDomain;
-        };
-        this.getIcon = function () {
-            return icon;
-        };
-        this.setIcon = function (newIcon) {
-            icon = newIcon;
-        };
-        this.getName = function () {
-            return name;
-        };
-        this.setName = function (newName) {
-            name = newName;
-        };
-        this.getCounter = function () {
-            return counter;
-        };
-        this.setCounter = function (newVal) {
-            counter = newVal;
-        };
-
-        this.setTimeSpent = function(newVal) {
-            timeSpent = newVal;
-        };
-
-        this.getTimeSpent = function() {
-            return timeSpent;
-        };
-
-        this.getCheckboxVal = function () {
-            return checkboxVal;
-        };
-        this.setCheckboxVal = function (newVal) {
-            checkboxVal = newVal;
-        };
-
+    constructor (urlBase, title) {
+        this._url = this.constructUrl(urlBase);
+        this._domain = urlBase;
+        this._name = title;
+        this._icon = constructIcon(urlBase);
+        this._checkboxVal = true;
+        this._counter = 0;
+        this._timeSpent = 0;
     }
 
-    return {
-        BlockedSite : BlockedSite,
-        serializeBlockedSite : serializeBlockedSite,
-        deserializeBlockedSite : deserializeBlockedSite,
-        parseBlockedSite : parseBlockedSite
-    }
+    constructUrl (url) {
+        return "*://" + url + "/*";
+    };
 
-});
+    constructIcon (url) {
+        return "<img style=\"-webkit-user-select: none\" src=\"" + constants.FAVICONLINK + url + "\">"
+    };
 
+    set url     (newUrl)    { this._url = newUrl }
+    get url     ()          { return this._url }
+
+    set domain  (newDomain) { this._domain = newDomain }
+    get domain  ()          { return domain }
+
+    set icon    (newIcon)   { this._icon = newIcon }
+    get icon    ()          { return this._icon }
+
+    set name    (newName)   { this._name = newName }
+    get name    ()          { return this._name }
+
+    set counter (newVal) { this._counter = newVal }
+    get counter () { return this._counter }
+
+    set timeSpent (newVal) { this._timeSpent = newVal }
+    get timeSpent () { return this._timeSpent }
+
+    set checkboxVal (newVal) { this._checkboxVal = newVal }
+    get checkboxVal () { return this._checkboxVal }
+
+}
