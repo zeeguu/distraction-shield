06b0f9d7f4dea63b71d0cadbc28b66347d7aefa4
Edser Apperloo <gamesedser@hotmail.com>
2017-05-12 20:56:17 +0200

    small update in the classes, merged serializer functions into the class

--- Distraction Shield/classes/BlockedSite.js
+++ Distraction Shield/classes/BlockedSite.js
@@ -1,47 +1,10 @@
 import * as constants from '../constants';
 
-/* --------------- --------------- Serialization --------------- --------------- */
-//Private to this and storage.js
-export function serializeBlockedSite (blockedSite) {
-    let obj = {
-        url: blockedSite.url,
-        domain: blockedSite.domain,
-        name: blockedSite.name,
-        icon: blockedSite.icon,
-        checkboxVal: blockedSite.checkboxVal,
-        counter: blockedSite.counter,
-        timeSpent: blockedSite.timeSpent
-    };
-    return JSON.stringify(obj);
-}
-
-//Private to this and blocked_site_list
-export function parseBlockedSite (blockedSite) {
-    let b = new BlockedSite();
-    b.url = blockedSite.url;
-    b.domain = blockedSite.domain;
-    b.name = blockedSite.name;
-    b.icon = blockedSite.icon;
-    b.checkboxVal = blockedSite.checkboxVal;
-    b.counter = blockedSite.counter;
-    b.timeSpent = blockedSite.timeSpent;
-    return b;
-}
-
-//Private to this and storage.js
-export function deserializeBlockedSite (serializedBlockedSite) {
-    if (serializedBlockedSite !== null) {
-        let parsed = JSON.parse(serializedBlockedSite);
-        return parseBlockedSite(parsed);
-    }
-    return null;
-}
-
 /* --------------- --------------- --------------- --------------- --------------- */
 
 export default class BlockedSite {
 
-    constructor (urlBase, title) {
+    constructor(urlBase, title) {
         this._url = this.constructUrl(urlBase);
         this._domain = urlBase;
         this._name = title;
@@ -51,33 +14,71 @@ export default class BlockedSite {
         this._timeSpent = 0;
     }
 
-    constructUrl (url) {
+    constructUrl(url) {
         return "*://" + url + "/*";
     };
 
-    constructIcon (url) {
+    constructIcon(url) {
         return "<img style=\"-webkit-user-select: none\" src=\"" + constants.FAVICONLINK + url + "\">"
     };
 
-    set url         (url)           { this._url = url }
-    get url         ()              { return this._url }
+    set url(url) { this._url = url; }
+    get url() { return this._url; }
 
-    set domain      (domain)        { this._domain = domain }
-    get domain      ()              { return this._domain }
+    set domain(domain) { this._domain = domain; }
+    get domain() { return this._domain; }
 
-    set icon        (icon)          { this._icon = icon }
-    get icon        ()              { return this._icon }
+    set icon(icon) { this._icon = icon; }
+    get icon() { return this._icon; }
 
-    set name        (name)          { this._name = name }
-    get name        ()              { return this._name }
+    set name(name) { this._name = name; }
+    get name() { return this._name; }
 
-    set counter     (counter)       { this._counter = counter }
-    get counter     ()              { return this._counter }
+    set counter(counter) { this._counter = counter; }
+    get counter() { return this._counter; }
 
-    set timeSpent   (timeSpent)     { this._timeSpent = timeSpent }
-    get timeSpent   ()              { return this._timeSpent }
+    set timeSpent(timeSpent) { this._timeSpent = timeSpent; }
+    get timeSpent() { return this._timeSpent; }
 
-    set checkboxVal (checkboxVal)   { this._checkboxVal = checkboxVal }
-    get checkboxVal ()              { return this._checkboxVal }
+    set checkboxVal(checkboxVal) { this._checkboxVal = checkboxVal; }
+    get checkboxVal() { return this._checkboxVal; }
+
+
+    /* --------------- --------------- Serialization --------------- --------------- */
+
+    static serializeBlockedSite(blockedSite) {
+        // let obj = {
+        //     url: blockedSite.url,
+        //     domain: blockedSite.domain,
+        //     name: blockedSite.name,
+        //     icon: blockedSite.icon,
+        //     checkboxVal: blockedSite.checkboxVal,
+        //     counter: blockedSite.counter,
+        //     timeSpent: blockedSite.timeSpent
+        // };
+        return JSON.stringify(blockedSite); //TODO check if this works
+    }
+
+
+    static parseBlockedSite(blockedSite) {
+        let b = new BlockedSite();
+        b.url = blockedSite.url;
+        b.domain = blockedSite.domain;
+        b.name = blockedSite.name;
+        b.icon = blockedSite.icon;
+        b.checkboxVal = blockedSite.checkboxVal;
+        b.counter = blockedSite.counter;
+        b.timeSpent = blockedSite.timeSpent;
+        return b;
+    }
+
+
+    static deserializeBlockedSite(serializedBlockedSite) {
+        if(serializedBlockedSite != null) {
+            let parsed = JSON.parse(serializedBlockedSite);
+            return this.parseBlockedSite(parsed);
+        }
+        return null;
+    }
 
 }
