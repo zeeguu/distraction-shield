eda398a34fd984dffc30139f348d4d0b279ac95f
Mark <marktmmrmn@gmail.com>
2017-06-09 15:54:46 +0200

    Tracker continued counting when you closed the chrome window when you were on a blocked site / exercise page

--- Distraction Shield/modules/statistics/tracker.js
+++ Distraction Shield/modules/statistics/tracker.js
@@ -137,10 +137,13 @@ export default class Tracker {
     addOnActiveTabChangeListener() {
         chrome.tabs.onActivated.addListener((activeInfo) => {
             chrome.tabs.get(activeInfo.tabId, (tab) => {
-                if(!chrome.runtime.lastError) {
+                if(typeof tab !== 'undefined') {
                     this.triggerUpdateTime();
                     this.currentTab = tab;
+                } else {
+                    this.currentTab = null;
                 }
+                if(chrome.runtime.lastError) return;
             })
         });
     }
