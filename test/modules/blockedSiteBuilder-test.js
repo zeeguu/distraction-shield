3dff6a160801612263ccc8a810a9e1f02a5c1353
pieterDekker <pieter.dekker20@gmail.com>
2017-06-10 18:09:39 +0200

    merge again, now with all of dev

--- test/modules/blockedSiteBuilder-test.js
+++ test/modules/blockedSiteBuilder-test.js
@@ -3,15 +3,27 @@
 /* eslint-env node */
 var assert = require('assert');
 var blockedSiteBuilder = require('../../dist/modules/blockedSiteBuilder');
+var sinon = require('sinon');
 
 
-describe('BlockedSite | unit tests', function() {
+describe('BlockedSiteBuilder | unit tests', function() {
     before(function() {
-
+        global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
     });
 
-    it('', function() {
-
+    it('should get a complete url from the hosting server', function(done) {
+        blockedSiteBuilder.createNewBlockedSite("facebook.com").then(
+            function (blockedSite) {
+                //resolution
+                console.log("ok");
+                assert.ok(/http[s]?:[/]{2}([\w]*.)*/.test(blockedSite));
+                done();
+            },
+            function (error) {
+                //rejection
+                console.log(error);
+                done();
+            });
     });
 
 });
