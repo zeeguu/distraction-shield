2848d74b43b43d37be2c6250480e44d398d87db7
Dunnkers <dunnkers@hotmail.com>
2017-06-11 06:32:04 +0200

    Deploy by script instead of bash 🦑

---
+++ scripts/deploy-gh-pages.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+let ghpages = require('gh-pages');
+let git = require('git-rev-sync');
+let chalk = require('chalk');
+
+console.log(chalk.green('Publishing docs...'));
+
+ghpages.publish('docs', {
+  message: `Deploy ${git.short()} from ${git.branch()}`,
+  push: false
+}, (err) => {
+  if (err) throw Error(err);
+
+  console.log(chalk.green('Docs successfully published!'));
+});
