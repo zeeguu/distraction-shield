9c5d4f54f88be9325e7582b29b9a4122bd429401
Jeroen Overschie <dunnkers@Elendil.local>
2019-05-29 00:34:04 +0200

    It works! 🎉

--- public/background.js
+++ public/background.js
@@ -1,18 +1,27 @@
 chrome.storage.onChanged.addListener(changes => {
+    console.debug('chrome.storage.onChanged', changes);
     setup();
 });
 
-
 function setup() {
+    console.debug('setup()');
+
     // remove
     chrome.webRequest.onBeforeRequest.removeListener(handleInterception);
 
     chrome.storage.sync.get(['blockedUrls'], function(result) {
+        console.debug(result.blockedUrls);
+        if (!result.blockedUrls) return; // no urls to be blocked.
+
         // add
         chrome.webRequest.onBeforeRequest.addListener(
             handleInterception
             , {
+                // urls should be of type
+                // https://developer.chrome.com/extensions/match_patterns
+                // 
                 urls: result.blockedUrls
+                //*://*.instagram.com/* // format example.
                 , types: ["main_frame"]
             }
             , ["blocking"]
@@ -26,3 +35,5 @@ function handleInterception(details) {
         redirectUrl: 'https://www.zeeguu.org/'
     };
 }
+
+setup();
