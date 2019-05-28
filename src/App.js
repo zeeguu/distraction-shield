0fd25059fb597d27199d2767793241744312c6d6
Jeroen Overschie <dunnkers@RN-145-90-128-34.eduroam.rug.nl>
2019-05-28 16:01:25 +0200

    Update layout 💎

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './aikido.png';
 import './App.css';
+// import { Switch } from 'antd';
 
 function App() {
   return (
@@ -8,16 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Distraction Shield
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Switch /> */}
       </header>
     </div>
   );
