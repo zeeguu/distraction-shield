894fabd8db744bd8e8356555dcc3dfe438c42e22
Jeroen Overschie <dunnkers@RN-145-90-128-34.eduroam.rug.nl>
2019-05-30 09:52:21 +0200

    Create options page 😇

---
+++ src/Options.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Options() {
+  return (
+    <div className="Options">
+      <header className="Options-header">
+        <p>
+          Distraction Shield
+        </p>
+      </header>
+      <p>Options page.</p>
+    </div>
+  );
+}
+
+export default Options;
