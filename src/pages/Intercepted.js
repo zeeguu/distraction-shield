ebca785b7f2e72a2219cfe37b89548a8517ea906
dunnkers <jeroenoverschie@gmail.com>
2019-06-03 21:59:29 +0200

    Improve interception page layout

--- src/pages/Intercepted.js
+++ src/pages/Intercepted.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import { Progress, message, Icon, Row, Col, Button } from 'antd';
 
 function Intercepted() {
+    message.open({
+        content: 'You were intercepted!',
+        icon: <Icon type="stop" />
+    });
+
     let params = (new URL(window.location)).searchParams; // since chrome 51, no IE
     let url = params.get('url');
 
     return (
         <div>
-            <h1>You were intercepted!</h1>
-
             <iframe title="Interception page" 
-                width="100%" height={800} src="https://www.zeeguu.org/">
+                width="100%"
+                src="https://www.zeeguu.org/"
+                style={{ height: '90vh'}}>
             </iframe>
-            <h4>You tried to visit {url}...</h4>
-            <a href={url}>Continue anyway...</a>
+            <div style={{ height: '9vh' }}>
+                <Row type="flex" justify="space-around" align="middle">
+                    <Col span={8} offset={6}>
+                        <h3>Exercises completed:</h3>
+                        <code>1 / 4</code>
+                        <Progress percent={25} />
+                    </Col>
+                    <Col span={2}>
+                        <a href={url}>
+                            <Button type="danger" icon="bell">
+                                Turn off for 10 minutes
+                            </Button>
+                        </a>
+                    </Col>
+                </Row>
+            </div>
+            
         </div>
     );
 }
