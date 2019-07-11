ef4f89c5e0d4a2e0ae656ef3717433fb3eba7b37
dunnkers <jeroenoverschie@gmail.com>
2019-07-11 10:48:40 +0200

    Refactor backgorund.js 💠

--- public/background.js
+++ public/background.js
@@ -1,8 +1,39 @@
-const timeoutHandlers = {};
+/* global chrome */
 
 chrome.storage.onChanged.addListener(changes => {
     setup();
 });
+
+const timeoutHandlers = {};
+
+function setTimeoutHandler(blockedUrl) {
+    resetTimeoutHandler(blockedUrl);
+
+    let { timeout, regex } = blockedUrl;
+    let timeleft = timeout - new Date().valueOf();
+    
+    timeoutHandlers[regex] = setTimeout(() => {
+        timeoutHandlers[regex] = null;
+        setup();
+    }, timeleft);
+}
+
+function resetTimeoutHandler(blockedUrl) {
+    if (timeoutHandlers[blockedUrl.regex]) {
+        clearTimeout(timeoutHandlers[blockedUrl.regex]);
+        timeoutHandlers[blockedUrl.regex] = null;
+    }
+}
+
+function isTimedOut(blockedUrl) {
+    if (blockedUrl.timeout && new Date().valueOf() <= blockedUrl.timeout) {
+        setTimeoutHandler(blockedUrl);
+        return false;
+    } else {
+        resetTimeoutHandler(blockedUrl);
+        return true;
+    }
+}
 
 function setup() {
     // remove
@@ -18,56 +49,16 @@ function setup() {
         // no urls to be blocked.
         if (!blockedUrls) return;
 
-        let willTimeout = [];
+        // only block urls that are not timed out.
         let urls = blockedUrls
-            // filters out blocked urls that currently have a timeout.
-            .filter(blockedUrl => {
-                let { timeout } = blockedUrl;
-
-                if (timeout && new Date().valueOf() <= timeout) {
-                    willTimeout.push(blockedUrl);
-                    return false;
-                } else {
-                    if (timeoutHandlers[blockedUrl.regex]) { // handler still set
-                        console.log('timeout cleared for ', blockedUrl.hostname);
-                        clearTimeout(timeoutHandlers[blockedUrl.regex]);
-                        timeoutHandlers[blockedUrl.regex] = null;
-                    }
-                    return true;
-                }
-            })
+            .filter(isTimedOut)
             .map(blockedUrl => blockedUrl.regex);
-        
-        // set timeout handlers
-        willTimeout.forEach(blockedUrl => {
-            let { timeout, regex } = blockedUrl;
-
-            if (timeoutHandlers[regex]) { // handler already set
-                clearTimeout(timeoutHandlers[blockedUrl.regex]);
-                timeoutHandlers[blockedUrl.regex] = null;
-            }
-
-            let timeleft = timeout - new Date().valueOf();
-            console.log('timeout set for ', blockedUrl.hostname);
-            timeoutHandlers[regex] = setTimeout(() => {
-                console.log('timeout triggered for ', blockedUrl.hostname);
-                setup();
-                timeoutHandlers[regex] = null;
-            }, timeleft);
-        });
 
         // add listener
-        chrome.webRequest.onBeforeRequest.addListener(
-            handleInterception
-            , {
-                // urls should be of type
-                // https://developer.chrome.com/extensions/match_patterns
-                // 
-                urls
-                //*://*.instagram.com/* // format example.
-                , types: ["main_frame"]
-            }
-            , ["blocking"]
+        chrome.webRequest.onBeforeRequest.addListener(handleInterception, {
+                urls, // https://developer.chrome.com/extensions/match_patterns
+                types: ["main_frame"]
+            }, ["blocking"]
         );
     });
 }
