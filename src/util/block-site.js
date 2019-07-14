b9842880283989c72fcfb38e75295b3237ee482e
Jeroen Overschie <jeroenoverschie@gmail.com>
2019-07-14 20:36:56 +0200

    Remove extraneous function 💎

--- src/util/block-site.js
+++ src/util/block-site.js
@@ -44,21 +44,6 @@ export const isCurrentWebsiteBlocked = () => {
     });
 };
 
-// // Currently not used..
-// export const isCurrentTab = () => {
-//     return new Promise((resolve) => {
-//         if (!(window.chrome && chrome.tabs)) return resolve(true); // no chrome env.
-
-//         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//             let tab = tabs[0];
-
-//             chrome.tabs.getCurrent(current => {
-//                 resolve(tab.id === current.id);
-//             });
-//         });
-//     });
-// }
-
 export const isWebsiteBlocked = async hostname => {
     const blockedUrls = await getWebsites();
     // sidenote: shouldn't we compare regex? let hostname be OK for now..
